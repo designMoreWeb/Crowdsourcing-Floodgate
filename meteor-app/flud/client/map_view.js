82cac305ae0a1c20cfa9ccb4bb04e654974bfee0
jeffm24 <jmariconda24@gmail.com>
2016-10-04 23:36:00 -0400

    - map_view:         - Implemented ability to click on the map and get a marker dropped at the nearest road (will be used for entering in datapoints)

--- meteor-app/flud/client/map_view.js
+++ meteor-app/flud/client/map_view.js
@@ -4,8 +4,12 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './views/map_view.html';
 
 Template.map_view.onCreated(function() {
+    var self = this;
+
     // counter starts at 0
     this.subscribe('datapoints');
+
+    this.addMarker = new ReactiveVar();
     
     // We can use the `ready` callback to interact with the map API once the map is ready.
     GoogleMaps.ready('exampleMap', function(map) {
@@ -13,6 +17,33 @@ Template.map_view.onCreated(function() {
         var marker = new google.maps.Marker({
             position: map.options.center,
             map: map.instance
+        });
+
+        google.maps.event.addListener(map.instance, 'click', function(event) {
+            $.ajax({
+                method: "GET",
+                url: "https://roads.googleapis.com/v1/snapToRoads",
+                data: { 
+                    path: event.latLng.lat() + ',' + event.latLng.lng(), 
+                    key: "AIzaSyAixYo-thWvStv30hqGZ9DZeT3IItN7atU" 
+                }
+            })
+            .done(function(msg) {
+                var currentAddMarker = self.addMarker.get();
+                var point = msg.snappedPoints[0];
+
+                if (currentAddMarker) {
+                    currentAddMarker.setMap(null);
+                }
+
+                self.addMarker.set(new google.maps.Marker({
+                    draggable: false,
+                    animation: google.maps.Animation.DROP,
+                    position: new google.maps.LatLng(point.location.latitude, point.location.longitude),
+                    map: map.instance,
+                    id: point.placeId
+                }));
+            });
         });
     });
 });
@@ -29,12 +60,12 @@ Template.map_view.helpers({
         if (GoogleMaps.loaded() && latLng) {
             return {
                 center: new google.maps.LatLng(latLng.lat, latLng.lng),
-                zoom: 8
+                zoom: 15
             };
         }
     }
 });
 
 Template.map_view.events({
-    
+
 });
